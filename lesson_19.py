0631c36b3ddf404eb6a9b1287bfe7e08373455af
unknown <SelinN@lcoast.itstep>
2025-02-17 19:26:17 +0500

    17.02

---
+++ lesson_19.py
@@ -0,0 +1,15 @@
+import pydantic
+
+class User(pydantic.BaseModel):
+    name: str
+    phone: int
+    year: int
+
+
+user = User(name="name", phone=111, year=2010)
+user_data = user.model_dump()
+print(user_data)
+# data = {"name": "name", "phone": 111, "year": 111}
+# user = User(**data)
+# json_data = '{"name": "name", "phone": 111, "year": 111}'
+# user = User.model_validate_json(json_data)
