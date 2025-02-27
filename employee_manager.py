4916331426d2c711277ce7e2e5510d80bae6b8d1
unknown <SelinN@lcoast.itstep>
2025-02-27 19:21:39 +0500

    beginning 2

--- employee_manager.py
+++ employee_manager.py
@@ -19,7 +19,14 @@ import json
 # Имя файла для хранения данных
 DATA_FILE = "employees.json"
 
+
+##### Хранение данных
+
 def load_data():
+    """
+    Читает данные из JSON-файла и загружает список сотрудников
+    в виде поддерживаемых python типов данных
+    """
     try:
         with open(DATA_FILE, "r") as file:
             return json.load(file) # Преобразование JSON из файла в
@@ -27,4 +34,20 @@ def load_data():
     except FileNotFoundError:
         return []
 
+
+def save_data(data):
+    """
+    Принимает в качестве аргумента list (data) и сохраняет его в виде
+    json в соответствующий файл
+    """
+    with open(DATA_FILE, "w") as file:
+        json.dump(data, file, indent=4)
+
+
+##### GUI-часть
+
+root = tk.Tk()
+root.title("Управление сотрудниками")
+
+frame = tk.Frame(root)
 
