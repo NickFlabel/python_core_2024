e229f620896ff7f0bb20e53db3125dcf45a61f10
unknown <SelinN@lcoast.itstep>
2025-02-27 19:43:16 +0500

    beginning 3

--- employee_manager.py
+++ employee_manager.py
@@ -14,6 +14,7 @@
 
 
 import tkinter as tk
+from tkinter import messagebox
 import json
 
 # Имя файла для хранения данных
@@ -50,4 +51,54 @@ root = tk.Tk()
 root.title("Управление сотрудниками")
 
 frame = tk.Frame(root)
+frame.pack(pady=10) # pady - расстояние от верха окна в пикселях
+
+# Поля для ввода информации
+tk.Label(frame, text="Имя:").grid(row=0, column=0)
+name_entry = tk.Entry(frame)
+name_entry.grid(row=0, column=1)
+
+tk.Label(frame, text="Должность:").grid(row=1, column=0)
+position_entry = tk.Entry(frame)
+position_entry.grid(row=1, column=1)
+
+tk.Label(frame, text="Зарплата:").grid(row=2, column=0)
+salary_entry = tk.Entry(frame)
+salary_entry.grid(row=2, column=1)
+
+def add_employee():
+    name = name_entry.get() # метод get() возвращает то, что пользователь
+    # ввел в соответствующее поле
+    position = position_entry.get()
+    salary = salary_entry.get()
+
+    if not name or not position or not salary:
+        messagebox.showerror("Ошибка", "Заполните все поля!")
+        return
+        # Открывает новое окно с ошибкой
+
+    try:
+        salary = float(salary)
+    except ValueError:
+        messagebox.showerror("Ошибка", "Зарплата должна быть числом!")
+        return
+
+    new_employee = {"name": name, "position": position, "salary": salary}
+    employees = load_data()
+    employees.append(new_employee)
+    save_data(employees)
+
+    # Удаляем уже ненужные введенные данные
+    name_entry.delete(0, tk.END)
+    position_entry.delete(0, tk.END)
+    salary_entry.delete(0, tk.END)
+
+tk.Button(
+    frame,
+    text="Добавить",
+    command=add_employee
+).grid(row=3, columnspan=2, pady=5) # columnspan - сколько колонок занимает
+# элемент
+
+root.mainloop()
 
